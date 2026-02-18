3f66a80336edb45d3742240ead9aeb9b3ca6bb8b
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-02-18 12:04:44 +0000

    Add IMetier interface and its implementation and use loose coopling

---
+++ src/main/java/com/nabgha/metier/IMetier.java
@@ -0,0 +1,5 @@
+package com.nabgha.metier;
+
+public interface IMetier {
+    double calcul();
+}
---
+++ src/main/java/com/nabgha/metier/MetierImpl.java
@@ -0,0 +1,17 @@
+package com.nabgha.metier;
+
+import com.nabgha.dao.IDao;
+
+public class MetierImpl implements IMetier {
+    IDao dao; // couplage faible
+
+    // Injection des dependences via le constructeur
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+    @Override
+    public double calcul() {
+        double t = dao.getData();
+        return t * 43 / 3;
+    }
+}
